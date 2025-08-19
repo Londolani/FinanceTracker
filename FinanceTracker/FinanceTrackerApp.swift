0d6b2ac4369653f9f57dcae746241ef52c036cb2
Londolani <londolani0700@gmail.com>
2025-08-19 21:45:38 +0200

    Initial Commit

---
+++ FinanceTracker/FinanceTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinanceTrackerApp.swift
+//  FinanceTracker
+//
+//  Created by Londolani Ndou on 2025/08/19.
+//
+
+import SwiftUI
+
+@main
+struct FinanceTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
